664002ff671c917b3fa6bfa26c8f24b22e7b5bcd
Zeqli <l.caecar@gmail.com>
2017-04-08 00:54:27 -0500

    Formatted class

--- src/main/java/edu/utdallas/project3/protocol/LamportClock.java
+++ src/main/java/edu/utdallas/project3/protocol/LamportClock.java
@@ -2,19 +2,28 @@ package edu.utdallas.project3.protocol;
 
 public class LamportClock {
     int timestamp;
+    
+    
     public LamportClock() {
         timestamp = 1;
     }
+    
     public int getValue() {
         return timestamp;
     }
+    
+    
     public void tick() { // on internal actions
         timestamp = timestamp + 1;
     }
+    
+    
     public void sendAction() {
        // include c in message
         timestamp = timestamp + 1;
     }
+    
+    
     public void receiveAction(int src, int sentValue) {
         timestamp = Math.max(timestamp, sentValue) + 1;
     }
